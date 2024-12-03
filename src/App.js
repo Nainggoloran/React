d16aa6a793eff08d106e7856d7c2b8a7311c7959
020706/NA <dkswlgh555@naver.com>
2024-12-03 13:54:34 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
+import Calc1 from './comp/calc/study01'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Home />} />
+          <Route path={"/about"} element={<About />} />
+          <Route path={"/calc"} element={<Calc1 />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+
+function About() {
+  return(
+    <div>
+      <h1>About</h1>
+      <Link to="/">Home으로 이동</Link>
+    </div>
+  )
+}
+
+function Home() {
+  return(
+    <div>
+      <h1>Start Home</h1>
+      <Link to="/about">About으로 이동</Link>
+      <Link to="/calc">Calc로 이동</Link>
+    </div>
+  )
+}
+
+
+
 
 export default App;
